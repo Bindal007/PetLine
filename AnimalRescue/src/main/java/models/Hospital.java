bcd19be73d3774da57c6c41b3c99e46f65df8157
Vicken Matiossian <vicken107@yahoo.com>
2022-12-06 17:47:30 -0500

    hospital class added

--- AnimalRescue/src/main/java/models/Hospital.java
+++ AnimalRescue/src/main/java/models/Hospital.java
@@ -4,10 +4,58 @@
  */
 package models;
 
+import java.util.ArrayList;
+
 /**
  *
  * @author vicken
  */
 public class Hospital {
+    
+    private String name;
+    private String about;
+    private int zipCode;
+    private String address;
+    
+    public Hospital(String name, String about, int zipCode,
+            String address){
+        this.name = name;
+        this.about = about;
+        this.zipCode = zipCode;
+        this.address = address;
+    }
+    
+    public void setName(String name){
+        this.name = name;
+    }
+    
+    public String getName(){
+        return name;
+    }
+    
+    public void setAbout(String about){
+        this.about = about;
+    }
+    
+    public String getAbout(){
+        return about;
+    }
+    
+    public void setAddress(String address){
+        this.address = address;
+    }
+    
+    public String getAddress(){
+        return address;
+    }
+    
+    public void setZip(int zipCode){
+        this.zipCode = zipCode;
+    }
+    
+    public int getZipCode(){
+        return zipCode;
+    }
+    
     
 }
