e3ce849d978c46b5c49dc9e913b347528f5a9597
Vicken Matiossian <vicken107@yahoo.com>
2022-12-08 16:29:28 -0500

    NGO model class completed

--- AnimalRescue/src/main/java/models/NGO.java
+++ AnimalRescue/src/main/java/models/NGO.java
@@ -10,4 +10,68 @@ package models;
  */
 public class NGO {
     
+    private int ngoID; // primary key
+    private String ngoName;
+    private int addressID; // foreign key
+    private int phoneNum;
+    private int numOfPets;
+    private int postID; // foreign key
+    
+    public NGO(int ngoID, String ngoName, int addressID, int phoneNum, int numOfPets, int postID) {
+        this.ngoID = ngoID;
+        this.ngoName = ngoName;
+        this.addressID = addressID;
+        this.phoneNum = phoneNum;
+        this.numOfPets = numOfPets;
+        this.postID = postID;
+    }
+    
+    public int getNGOID() {
+        return ngoID;
+    }
+    
+    public void setNGOID(int ngoID) {
+        this.ngoID = ngoID;
+    }
+
+    public String getNGOName() {
+        return ngoName;
+    }
+    
+    public void setNGOName(String ngoName) {
+        this.ngoName = ngoName;
+    }
+
+    public int getAddressID() {
+        return addressID;
+    }
+    
+    public void setAddressID(int addressID) {
+        this.addressID = addressID;
+    }
+
+    public int getPhoneNum() {
+        return phoneNum;
+    }
+    
+    public void setPhoneNum(int phoneNum) {
+        this.phoneNum = phoneNum;
+    }
+
+    public int getNumOfPets() {
+        return numOfPets;
+    }
+    
+    public void setNumOfPets(int numOfPets) {
+        this.numOfPets = numOfPets;
+    }
+
+    public int getPostID() {
+        return postID;
+    }
+    
+    public void setPostID(int postID) {
+        this.postID = postID;
+    }
+    
 }
