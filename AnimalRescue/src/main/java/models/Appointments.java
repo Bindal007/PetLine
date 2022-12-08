bc1b6fce37155e3db0867ad0536c36cb26438ce7
Vicken Matiossian <vicken107@yahoo.com>
2022-12-08 16:24:16 -0500

    appointments model class completed

--- AnimalRescue/src/main/java/models/Appointments.java
+++ AnimalRescue/src/main/java/models/Appointments.java
@@ -13,6 +13,72 @@ public class Appointments {
     private String dateTime;
     private String reason;
     private String petType;
-    // private Vitals vitals;
+    private Vitals vitals;
+    
+    public Appointments(int appointmentID, int vetID, int personID, String dateTime, String reason, String petType, Vitals vitals) {
+        this.appointmentID = appointmentID;
+        this.vetID = vetID;
+        this.personID = personID;
+        this.dateTime = dateTime;
+        this.reason = reason;
+        this.petType = petType;
+        this.vitals = vitals;
+    }
+    
+    public int getAppointmentID() {
+        return appointmentID;
+    }
+    
+    public void setAppointmentID(int appointmentID) {
+        this.appointmentID = appointmentID;
+    }
+
+    public int getVetID() {
+        return vetID;
+    }
+    
+    public void setVetID(int vetID) {
+        this.vetID = vetID;
+    }
+
+    public int getPersonID() {
+        return personID;
+    }
+    
+    public void setPersonID(int personID) {
+        this.personID = personID;
+    }
+
+    public String getDateTime() {
+        return dateTime;
+    }
+    
+    public void setDateTime(String dateTime) {
+        this.dateTime = dateTime;
+    }
+
+    public String getReason() {
+        return reason;
+    }
+    
+    public void setReason(String reason) {
+        this.reason = reason;
+    }
+
+    public String getPetType() {
+        return petType;
+    }
+    
+    public void setPetType(String petType) {
+        this.petType = petType;
+    }
+
+    public Vitals getVitals() {
+        return vitals;
+    }
+    
+    public void setVitals(Vitals vitals) {
+        this.vitals = vitals;
+    }
     
 }
