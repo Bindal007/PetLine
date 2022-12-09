7c6e61a52e2a5d956d705e20ac15a3de978909f0
ZubearFa <zubair.f@northeastern.edu>
2022-12-09 02:10:26 -0500

    Mysql database connection created

--- AnimalRescue/src/main/java/database/Database.java
+++ AnimalRescue/src/main/java/database/Database.java
@@ -3,11 +3,26 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
  */
 package database;
-
+import java.sql.Connection;
+import java.sql.DriverManager;
 /**
  *
  * @author nikhilbindal
  */
 public class Database {
+    public static Connection getConnect() {
+        Connection conn = null;
+        try {
+            conn = DriverManager.getConnection("jdbc:mysql://localhost:3306/petline?user=root&password=root");
+            System.out.println("Connection established!");
+        }
+        catch (Exception e) {
+            e.printStackTrace();
+        }
+        return conn;
+    }
     
+    public static void main(String args[]) {
+        getConnect();
+    }
 }
